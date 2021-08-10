4a427fef291697f0861fe05e57c1524803f3e41d
Samuel <sgmin206@naver.com>
2021-08-10 11:13:57 +0900

    Update middleware.js
    locals object는 이미 모든 pug template에 import 된 object이다.

--- src/middleware.js
+++ src/middleware.js
@@ -2,5 +2,8 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
+  if (res.locals.loggedInUser.name === null) {
+    res.locals.loggedInUser.name = "unknown";
+  }
   next();
 };
